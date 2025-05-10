16be3b05af20d5a64f909f9dec29ec905d728715
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2025-05-10 16:32:57 +0200

    chore: 1st commit, basic stuff and github actions

---
+++ app/src/main/java/com/rpla/marsrovernavigator/MainActivity.kt
@@ -0,0 +1,52 @@
+@file:Suppress("ktlint:standard:function-naming")
+
+package com.rpla.marsrovernavigator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rpla.marsrovernavigator.ui.theme.MarsRoverNavigatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MarsRoverNavigatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MarsRoverNavigatorTheme {
+        Greeting("Android")
+    }
+}
