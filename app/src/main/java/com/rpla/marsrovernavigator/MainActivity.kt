52841815e4b293f93ed1dbccd2c502510b2fe7f2
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2025-05-19 11:06:09 +0200

    resources: add splash screen implementation

--- app/src/main/java/com/rpla/marsrovernavigator/MainActivity.kt
+++ app/src/main/java/com/rpla/marsrovernavigator/MainActivity.kt
@@ -13,10 +13,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.rpla.marsrovernavigator.ui.theme.MarsRoverNavigatorTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
